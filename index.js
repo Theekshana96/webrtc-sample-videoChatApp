f31c4d328e3f72a4e9100be28ddd76a87b18b38f
Dilshan Theekshana <theekshanajayarathne@gmail.com>
2022-06-10 11:39:16 +0530

    room is changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 const io = socket(server);
 
 io.on('connection', (socket) => {
-  console.log(`User connected: ${socket.id}`);
+  console.log(`User is connected: ${socket.id}`);
 
   socket.on('join', (roomName) => {
     const rooms = io.sockets.adapter.rooms;
